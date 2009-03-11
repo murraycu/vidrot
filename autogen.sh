210aa433dedbc412cb0a35a9e3a042372f11df2b
David King <davidk@openismus.com>
2009-03-11 11:00:53 +0100

    * Makefile.am:         * configure.ac:         * m4/dk-warn.m4: Rename GTKMM_* to VIDROT_* for clarity. Use         Daniel's dk-warn.m4 automagic warning macro.         * autogen.sh: Force m4 include to aclocal.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,10 @@
 #!/bin/sh -e
- 
-autoreconf --install
+
+${ACLOCAL-aclocal} -I m4 $ACLOCAL_FLAGS
+${AUTOCONF-autoconf}
+${AUTOHEADER-autoheader}
+${AUTOMAKE-automake} --add-missing --copy $AUTOMAKE_FLAGS
+
+# autoreconf fails to include the m4 folder. Lame.
+#autoreconf --install
 ./configure "$@"
